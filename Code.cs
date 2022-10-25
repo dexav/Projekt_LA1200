1b287b261b1b585aa49dd57986939ac3b47824bd
DBDEnjoyer <110892739+DBDEnjoyer@users.noreply.github.com>
2022-10-25 09:15:16 +0200

    mer bruchet no arrays

--- Code.cs
+++ Code.cs
@@ -1,1 +1,24 @@
 
+Console.WriteLine("Herzlich Willkommen zur Englisch Voci abfrage!");
+
+
+            bool wiederhohlung = true;
+            if (wiederhohlung == true)
+            {
+            int nummer = rnd.Next(1, 21);
+            Console.WriteLine("übersetze ins englisch:");
+            Console.WriteLine(deutsch[nummer]);
+            antwort[nummer] = Console.ReadLine();
+            if (antwort[nummer] == englisch[nummer])
+            {
+                Console.WriteLine("du hast es richtig übersetzt ");
+            }
+            else
+            {
+                Console.WriteLine("schlecht gemacht du lappen");
+            }
+                Console.WriteLine("willst du nochmal spielen? true/false");
+                wiederhohlung = (Console.ReadLine());
+            }
+
+hat Kontextmenü
