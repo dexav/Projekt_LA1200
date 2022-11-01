b07f96b373e4165af6a7cb8705e1ad95b80f7d6e
dexav <110892637+dexav@users.noreply.github.com>
2022-11-01 09:59:43 +0100

    WAHL FÜR DEUTSCH ODER ENGLISCH ANTWOTEN

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LA1200_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string antowerten = "DE";
+            Console.WriteLine("Möchtest du auf English(EN) oder auf Deutsch(DE) antworten?");
+            string antwort = Console.ReadLine();
+
+            if (antwort == "DE")
+            {
+
+                Console.WriteLine("Sie antworten jetzt auf deutsch");
+            }else if (antwort == "EN")
+            {
+
+                Console.WriteLine ("Sie antworten jetzt auf english");
+            }
+
+
+
+        }   
+
+    }
+}
